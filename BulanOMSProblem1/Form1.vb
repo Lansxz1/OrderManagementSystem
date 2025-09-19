8d8b687f68ad6b6ce115753dc68537d0a16a65a9
kristoffer lance bulan <hed-kbulan@smu.edu.ph>
2025-09-19 09:58:55 +0800

    Commit

--- BulanOMSProblem1/Form1.vb
+++ BulanOMSProblem1/Form1.vb
@@ -1,34 +1,38 @@
 ﻿Public Class Form1
     Private Sub btnOrder_Click(sender As Object, e As EventArgs) Handles btnOrder.Click
-        Dim selectedBeverage = ComboBox1.SelectedItem.ToString()
-        Dim quantity = NumericUpDown1.Value
-        Dim total As Double
-        Dim subtotal As Double
-        Dim totalAmount As Double
-        Dim name As String = TextBox1.Text.Trim()
 
-        Select Case selectedBeverage
-            Case "Espresso: 120 PHP"
-                totalAmount = ((total) + 120 * quantity)
-            Case "Latte: 150 PHP"
-                totalAmount = ((total) + 150 * quantity)
-            Case "Cappuccino :  160 PHP"
-                totalAmount = ((total) + 160 * quantity)
-            Case "Americano: 130 PHP"
-                totalAmount = ((total) + 130 * quantity)
-            Case "Mocha: 170 PHP"
-                totalAmount = ((total) + 170 * quantity)
-                Exit Sub
-        End Select
-        lblCustomer.Text = "Customer Name: " & name
-        lblTotal.Text = "Final Total: " & totalAmount.ToString() & " PHP"
+        Try
+            Dim selectedBeverage = ComboBox1.SelectedItem.ToString()
+            Dim quantity = NumericUpDown1.Value
+            Dim total As Double
+            Dim subtotal As Double
+            Dim totalAmount As Double
+            Dim name As String = TextBox1.Text.Trim()
 
-        If (CheckBox1.Checked) Then
-            subtotal = totalAmount - 10%
-            lblSubtotal.Text = "Subtotal: " & subtotal.ToString() & " PHP with Discount"
-        End If
+            Select Case selectedBeverage
+                Case "Espresso: 120 PHP"
+                    totalAmount = ((total) + 120 * quantity)
+                Case "Latte: 150 PHP"
+                    totalAmount = ((total) + 150 * quantity)
+                Case "Cappuccino :  160 PHP"
+                    totalAmount = ((total) + 160 * quantity)
+                Case "Americano: 130 PHP"
+                    totalAmount = ((total) + 130 * quantity)
+                Case "Mocha: 170 PHP"
+                    totalAmount = ((total) + 170 * quantity)
+                    Exit Sub
+            End Select
+            lblCustomer.Text = "Customer Name: " & name
+            lblTotal.Text = "Final Total: " & totalAmount.ToString() & " PHP"
 
+            If (CheckBox1.Checked) Then
+                subtotal = totalAmount - 10%
+                lblSubtotal.Text = "Subtotal: " & subtotal.ToString() & " PHP with Discount"
+            End If
 
+        Catch
+            MsgBox("Input Beverage and Amount")
+        End Try
 
 
     End Sub
